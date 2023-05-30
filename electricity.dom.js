01e13b1be632866d18196e969f349a2138b01a61
Kabelo Madimabe <madimabekabelo@gmail.com>
2023-05-30 15:35:49 +0200

    Work in progress

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,59 +1,115 @@
 // DOM element(s) references
 
-// DOM references for electricity buying buttons 
+// DOM references for electricity buying buttons
 const radioBtnTen = document.getElementById("ten");
 const radioBtnTwenty = document.getElementById("twenty");
 const radioBtnFifty = document.getElementById("fifty");
 const radioBtnThirtyAdvance = document.getElementById("advance");
 const buyBtn = document.querySelector(".btn");
 
-// DOM references for electricity available displays 
+// DOM references for electricity available displays
 var unitsAvailableSpan = document.querySelector(".total.unitsAvailable");
 var totalUnitsBought = document.querySelector(".total.totalUnits");
 var totalAmountSpan = document.querySelector(".total.totalAmount");
+var advanceUsedTick = document.querySelector(".advanceUsedTick");
 
-// DOM references for electricity usage radio buttons 
+// DOM references for electricity usage radio buttons
+const radioBtnUseStove = document.getElementById("stove");
+const radioBtnUseTv = document.getElementById("tv");
+const radioBtnUseFridge = document.getElementById("fridge");
+const radioBtnUseKettle = document.getElementById("kettle");
+const useNowBtn = document.querySelector(".btn.useNow");
 
-const radioBtnUseStove = document.getElementById(".ten");
-const radioBtnUseTv = document.getElementById(".twenty");
-const radioBtnUseFridge= document.getElementById(".fifty");
-const radioBtnUseKettle = document.getElementById(".advance");
+// Electricity units and amounts
+var unitsAvailable = 0;
+var totalUnitsBoughtCount = 0;
+var totalAmountSpent = 0;
+var advanceUsed = false;
 
-// Factory Function instance 
-const electricity =  Electricity();
+// Factory Function instance
+const electricity = Electricity();
 
-
-// DOM events here 
-
+// Event listener for buying units
 buyBtn.addEventListener("click", function () {
-    if (radioBtnTen.checked) {
-      var currentUnitsAvailable = Number(unitsAvailableSpan.innerHTML);
-      updatedUnitsAvailable = currentUnitsAvailable + 7;
-      totalUnits = 7;
-      var amountSpent = 10; // R10.00 for buying 7 units
-    } else if (radioBtnTwenty.checked) {
-      var currentUnitsAvailable = Number(unitsAvailableSpan.innerHTML);
-      updatedUnitsAvailable = currentUnitsAvailable + 14;
-      totalUnits = 14;
-      var amountSpent = 20; // R20.00 for buying 14 units
-    } else if (radioBtnFifty.checked) {
-      var currentUnitsAvailable = Number(unitsAvailableSpan.innerHTML);
-      updatedUnitsAvailable = currentUnitsAvailable + 35;
-      totalUnits = 35;
-      var amountSpent = 50; // R50.00 for buying 35 units
-    } else if (radioBtnThirtyAdvance.checked) {
-      var currentUnitsAvailable = Number(unitsAvailableSpan.innerHTML);
-      updatedUnitsAvailable = currentUnitsAvailable + 21;
-      totalUnits = 21;
-      var amountSpent = 30; // R30.00 for buying 21 units
+  var updatedUnitsAvailable = 0;
+  var totalUnits = 0;
+  var amountSpent = 0;
+
+  if (radioBtnTen.checked) {
+    updatedUnitsAvailable = 7;
+    totalUnits = 7;
+    amountSpent = 10; // R10.00 for buying 7 units
+  } else if (radioBtnTwenty.checked) {
+    updatedUnitsAvailable = 14;
+    totalUnits = 14;
+    amountSpent = 20; // R20.00 for buying 14 units
+  } else if (radioBtnFifty.checked) {
+    updatedUnitsAvailable = 35;
+    totalUnits = 35;
+    amountSpent = 50; // R50.00 for buying 35 units
+  } else if (radioBtnThirtyAdvance.checked) {
+    if (advanceUsed) {
+      alert("Advance already used. Pay back the advance to use again.");
+      return;
     }
-  
-    unitsAvailableSpan.innerHTML = updatedUnitsAvailable;
-    totalUnitsBought.innerHTML = totalUnits;
-  
-    var currentAmountSpent = Number(totalAmountSpan.innerHTML);
-    var totalAmount = currentAmountSpent + amountSpent;
-    totalAmountSpan.innerHTML = totalAmount.toFixed(2);
+    updatedUnitsAvailable = 21;
+    totalUnits = 21;
+    amountSpent = 30; // R30.00 for buying 21 units
+    advanceUsed = true;
+    advanceUsedTick.style.display = "inline-block";
+  }
+
+  unitsAvailable += updatedUnitsAvailable;
+  totalUnitsBoughtCount += totalUnits;
+  totalAmountSpent += amountSpent;
+
+  updateUnitsAvailable();
+  updateTotalUnitsBought();
+  updateTotalAmountSpent();
+});
+
+// Event listeners for appliance usage
+radioBtnUseStove.addEventListener("click", function () {
+    useAppliance("Stove", 10);
+  });
+  radioBtnUseTv.addEventListener("click", function () {
+    useAppliance("TV", 3);
+  });
+  radioBtnUseFridge.addEventListener("click", function () {
+    useAppliance("Fridge", 13);
+  });
+  radioBtnUseKettle.addEventListener("click", function () {
+    useAppliance("Kettle", 5);
   });
   
+  // Helper function to update the units available display
+  function updateUnitsAvailable() {
+    unitsAvailableSpan.innerHTML = unitsAvailable;
+  }
+  
+  // Helper function to update the total units bought display
+  function updateTotalUnitsBought() {
+    totalUnitsBought.innerHTML = totalUnitsBoughtCount;
+  }
+  
+  // Helper function to update the total amount spent display
+  function updateTotalAmountSpent() {
+    totalAmountSpan.innerHTML = totalAmountSpent.toFixed(2);
+  }
+  
+  // Helper function to use an appliance
+  function useAppliance(appliance, units) {
+    if (unitsAvailable >= units) {
+      unitsAvailable -= units;
+      updateUnitsAvailable();
+      totalUnitsBoughtCount += units;
+      updateTotalUnitsBought();
+      var currentAmountSpent = Number(totalAmountSpan.innerHTML);
+      var amountSpent = units * 2; // Assuming each unit costs R2.00
+      totalAmountSpent += amountSpent;
+      updateTotalAmountSpent();
+    } else {
+      alert("Not enough electricity units available.");
+    }
+  }
   
--- electricity.js
+++ electricity.js
@@ -1,46 +1,67 @@
 function Electricity() {
-
-    // do we want to go with this or array? 
     let appliances = {
-        'Stove': 10, 
-        'Kettle': 5, 
-        'TV': 3, 
-        'Fridge': 13
+      Stove: 10,
+      Kettle: 5,
+      TV: 3,
+      Fridge: 13,
     };
-
+  
+    let unitsAvailable = 0;
+    let totalUnitsBoughtCount = 0;
+    let totalAmountSpent = 0;
+    let advanceUsed = false;
+  
     function topUpElectricity(amount) {
-
-
+      if (amount === "advance") {
+        if (advanceUsed) {
+          alert("Advance already used. Pay back the advance to use again.");
+          return;
+        }
+        unitsAvailable += 21;
+        totalUnitsBoughtCount += 21;
+        totalAmountSpent += 30;
+        advanceUsed = true;
+      } else {
+        unitsAvailable += amount / 2;
+        totalUnitsBoughtCount += amount / 2;
+        totalAmountSpent += amount;
+      }
     }
-
+  
     function getUnitsAvailable() {
-         return unitsAvailable;
+      return unitsAvailable;
     }
-
-    /*
-    * return true and substract from unit available if there is enough units to use the appliance
-    * other wise return false and do nothing.
-    */
+  
     function useAppliance(appliance) {
-        
+      if (unitsAvailable >= appliances[appliance]) {
+        unitsAvailable -= appliances[appliance];
+        totalUnitsBoughtCount += appliances[appliance];
+        totalAmountSpent += appliances[appliance] * 2;
+        return true;
+      } else {
+        return false;
+      }
     }
-
+  
     function advanceTaken() {
+      return advanceUsed;
     }
-
+  
     function totalAmountSpent() {
+      return totalAmountSpent;
     }
-
-    function totalUnitsBought(){
+  
+    function totalUnitsBought() {
+      return totalUnitsBoughtCount;
     }
-
+  
     return {
-        advanceTaken,
-        topUpElectricity,
-        getUnitsAvailable,
-        useAppliance,
-        totalAmountSpent,
-        totalUnitsBought
-
-    }
-}
+      topUpElectricity,
+      getUnitsAvailable,
+      useAppliance,
+      advanceTaken,
+      totalAmountSpent,
+      totalUnitsBought,
+    };
+  }
+  
